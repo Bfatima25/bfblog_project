e178407afa4f65f4e085050b1629901f444bb778
Bfatima25 <bushrafatima102@gmail.com>
2022-03-16 01:33:38 -0500

    deployement steps complete from 1 to 5

--- bfblog/settings.py
+++ bfblog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,3 +145,5 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+django_on_heroku.settings(locals())
