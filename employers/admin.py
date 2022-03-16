174f6ee0d6f75cd8e97939d9ff2586992bb9e6ca
Bfatima25 <bushrafatima102@gmail.com>
2022-03-15 22:53:56 -0500

    customized admin display data

--- employers/admin.py
+++ employers/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Employer
 
-admin.site.register(Employer)
+class EmployerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 20
+
+admin.site.register(Employer, EmployerAdmin)
 
