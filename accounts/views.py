bb125570c1617e406787c9fdf034bee7d73a6929
Bfatima25 <bushrafatima102@gmail.com>
2022-03-20 21:57:28 -0500

    updated accounts views.py

--- accounts/views.py
+++ accounts/views.py
@@ -5,27 +5,33 @@ from django.contrib.auth.models import User
 def register(request):
     if request.method == 'POST':
         # Get form values
-        first_name=request.POST.get("first_name", "default value")
-        last_name=request.POST.get("last_name", "default value")
+        first_name=request.POST.get("first_name")
+        last_name=request.POST.get("last_name")
         username = request.POST['username']
-        email=request.POST.get("email", "default value")
-        password=request.POST.get("password", "default value")
-        password2=request.POST.get("password2", "default value")
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        password2=request.POST.get("password2")
 
         # check if passwords match
         if password == password2:
-            if User.objects.filter(username=username):
+            # Check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken')
                 return redirect('register')
             else:
-                if User.objects.filter(email=email):
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email is being used')
                     return redirect('register')
                 else:
                     user = User.objects.create_user(username=username, password=password, 
                     email=email, first_name=first_name, last_name=last_name)
-
-                    auth.login(request, user)
-                    messages.success(request, 'You are now logged in')
-                    return redirect('index')
+                    #Login after register
+                    #auth.login(request, user)
+                    #messages.success(request, 'You are now logged in')
+                    #return redirect('index')
+                    user.save()
+                    messages.success(request, 'You are now registered and can log in')
+                    return redirect('login')
         else:
             messages.error(request, 'passwords do not match')
             return redirect('register')
